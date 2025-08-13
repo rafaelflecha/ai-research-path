7732538e82235b2520c7a3f6f12586abfaf36af2
Rafael Flecha <rafael.flecha@gmail.com>
2025-08-13 19:42:24 -0300

    Refactor log analysis functionality into separate functions for improved readability and maintainability

--- month-01/week-01-log-analyzer/analyzer.py
+++ month-01/week-01-log-analyzer/analyzer.py
@@ -1,22 +1,32 @@
 from pathlib import Path
 
-infoCount = 0
-warningCount = 0
-errorCount = 0
+def analyze_logs(log_file_path):
+    log_counts = { "INFO": 0, "WARNING": 0, "ERROR": 0 }
+    
+    try:
+        with open(log_file_path, 'r') as file:
+            for line in file:
+                if "INFO" in line:
+                    log_counts["INFO"] += 1
+                elif "WARNING" in line:
+                    log_counts["WARNING"] += 1
+                elif "ERROR" in line:
+                    log_counts["ERROR"] += 1
+    except FileNotFoundError:
+        print(f"Error: File not found at {log_file_path}")
+        return None
+    
+    return log_counts
 
-path = Path('sample.log')
+def print_summary(counts):
+    if not counts:
+        return 
+    print("Log Level Analysis")
+    print("------------------")
+    for level, count in counts.items():
+        print(f"{level}: {count}")
 
-with path.open() as file:
-    for line in file:
-        if line.find("INFO") > -1: 
-            infoCount += 1
-        elif line.find("WARNING") > -1:
-            warningCount += 1
-        elif line.find("ERROR") > -1:
-            errorCount += 1     
-
-print("Log Level Analysis")
-print("------------------")
-print(f"INFO: {infoCount}")
-print(f"WARNING: {warningCount}")
-print(f"ERROR: {errorCount}")
+if __name__ =="__main__":
+    path = Path("sample.log")
+    counts = analyze_logs(path)
+    print_summary(counts)
